1c947fb122098e9433869a747a068b3689d02025
khizar-k <khiz_k@outlook.com>
2020-08-31 23:51:38 -0700

    airbnb update

--- airbnb (react)/README.md
+++ airbnb (react)/README.md
@@ -1,1 +1,3 @@
-# Airbnb Placeholder
+# Airbnb Placeholder (currently just front-end)
+
+![Live site](https://airbnb-clone-d6fe5.web.app/)
--- facebook (react)/messenger v2 (upgrade for deployment)/client/src/components/Chat/Chat.js
+++ facebook (react)/messenger v2 (upgrade for deployment)/client/src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
+  const ENDPOINT = 'https://khiz-chat-server.herokuapp.com/';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
