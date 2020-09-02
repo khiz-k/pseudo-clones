bf43d4784fc3fb1bc92ac061676b7e4b9a838a16
khizar-k <khiz_k@outlook.com>
2020-09-01 18:22:28 -0700

    youtube typescript

---
+++ youtube (react)/v2 (migrating to typescript/src/containers/SideBar/SideBarFooter/SideBarFooter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SideBarFooter.scss'
+
+export function SideBarFooter() {
+  return (
+    <React.Fragment>
+      <div className='footer-block'>
+        <div>© Khizar Khan </div>
+      </div>
+    </React.Fragment>
+  );
+}
---
+++ youtube (react)/v2 (migrating to typescript/src/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'semantic-ui-css/semantic.min.css';
+import { Provider } from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
+import {configureStore} from './store/configureStore';
+
+const store = configureStore();
+
+// window.oncontextmenu = () => false;
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </Provider>, document.getElementById('root'));
+
+
