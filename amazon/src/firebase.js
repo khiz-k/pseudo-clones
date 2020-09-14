f1f08aed4c9cc626730b5ffc8dcc90c57b471c76
Khizar Khan <khiz_k@outlook.com>
2020-09-13 23:24:23 -0700

    config update

--- amazon/src/firebase.js
+++ amazon/src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCS6ILsyaYBgSgfcjYQYkckKPW5aRapiOI",
-  authDomain: "clone-f1c46.firebaseapp.com",
-  databaseURL: "https://clone-f1c46.firebaseio.com",
-  projectId: "clone-f1c46",
-  storageBucket: "clone-f1c46.appspot.com",
-  messagingSenderId: "695874079140",
-  appId: "1:695874079140:web:ff355bf98894c395f39931",
-  measurementId: "G-YDR1W4RGN5",
+  // your details
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
